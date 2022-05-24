29b59d0c20b5333325521510f19a03e872650b96
Matias Minoni <99848052+MatiasMinoni@users.noreply.github.com>
2022-05-23 23:57:26 -0300

    Add files via upload

---
+++ material.js
@@ -0,0 +1,6 @@
+M.toast({html: 'I am a toast!'})
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, options);
+  });
